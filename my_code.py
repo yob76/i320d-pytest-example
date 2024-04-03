7c889d3bb3ab600191e75f4d5763096d42c324d2
James Howison <james@howison.name>
2024-04-03 13:24:44 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,8 +1,8 @@
 def fix_phone_num(phone_num_to_fix):
   # given "5125558823". Split the parts, then recombine and return
-  area_code = phone_num_to_fix[0:2] # 512 (first three digits)
-  three_part = phone_num_to_fix[3:5] # 555 (next three digits)
-  four_part = phone_num_to_fix[6:9] # # 8823 (last four digits)
+  area_code = phone_num_to_fix[0:3]  # 512 (first three digits)
+  three_part = phone_num_to_fix[3:6] # 555 (next three digits)
+  four_part = phone_num_to_fix[6:] # 8823 (last four digits)
   
   fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
   
