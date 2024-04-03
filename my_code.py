5e252e36c504710798a4882cb28913563882f5f7
James Howison <james@howison.name>
2024-04-03 14:03:24 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -10,3 +10,7 @@ def fix_phone_num(phone_num_to_fix):
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
+  
+  # Now check that a too short string gives a ValueError
+  with pytest.raises(ValueError):
+    fix_phone_num("51")
